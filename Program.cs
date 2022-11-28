42b3d25c254c6d39bb04e0ddeb4f2f7a2bc7ea03
20jonas02 <jogajdosik@gmail.com>
2022-11-28 17:11:15 +0100

    refactor to use System.Numerics.Complex

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Numerics;
 
 namespace jokubui_uzd {
     internal class Program {
@@ -7,10 +8,10 @@ namespace jokubui_uzd {
         static List<int> primes;
         static Random rand;
         static void Main() {
-            Zeta = new Complex() { Im = (Math.Sqrt(3) - 1) / 2d, Re = 0d };
+            Zeta = new( (Math.Sqrt(3) - 1) / 2d, 0d);
             rand = new();
             primes = Primes.GetPrimes(500, 1000);
-            for (int i = 0; i < 15; i++) {
+            for (int i = 0; i < 1; i++) {
                 DoCalc();
             }
             Console.ReadKey();
@@ -29,6 +30,7 @@ namespace jokubui_uzd {
             double coef = 2d;
             Console.WriteLine($"{n} = {p1} * {p2} * {p3}");
             Console.WriteLine($"guess is {ratio}");
+            Console.WriteLine($"b:{b}; c:{c}");
             int i;
             for (i = 0; i < 2000; i++) {
                 int num = 0;
@@ -36,15 +38,15 @@ namespace jokubui_uzd {
                 double res = -9 * b * c - 27 * n;
                 double d1 = res + 2 * b * b * b;
                 double A = d1 * d1 - 4 * d0 * d0 * d0;
-                Complex bb = new() { Re = b };
-                Complex dd0 = new() { Re = d0 };
+                Complex bb = new(b, 0d);
+                Complex dd0 = new(d0,0d );
                 if (A >= 0) {
                     double A1 = Math.Sqrt(A);
                     double B = (d1 - A1) / 2;
-                    Complex C = new() { Re = Math.Cbrt(B) };
+                    Complex C = new( Math.Cbrt(B), 0d );
                     Print(dd0, bb, C, ref num);
                 } else {
-                    Complex C = Complex.Pow(new(re: d1 / 2d,im:Math.Sqrt(-A) / -2d), 1d/3d);
+                    Complex C = Complex.Pow(new(d1 / 2d,Math.Sqrt(-A) / -2d), 1d/3d);
                     Print(dd0, bb, C, ref num);
                 }
                 double rat_now = Math.Abs(c / (double)b);
@@ -60,6 +62,7 @@ namespace jokubui_uzd {
                         rat_now = Math.Abs(c / (double)b);
                     }
                 }
+                Console.WriteLine($"b:{b}; c:{c}");
                 if (num == 3) break;
             }
             if (i < 2000) {
@@ -72,8 +75,8 @@ namespace jokubui_uzd {
             for (int i = 0; i <= 2; i++) {
                 Complex zi = Complex.Pow(Zeta, i);
                 Complex x = (bb + zi * C + dd0 / (zi * C)) / -3;
-                if (x.Im < 0.001d && x.Im > -0.001d) {
-                    if (x.Re - FastFloor(x.Re) < 0.01) num++;
+                if (x.Imaginary < 0.001d && x.Imaginary > -0.001d) {
+                    if (x.Real - FastFloor(x.Real) < 0.01) num++;
                 }
             }
         }
@@ -103,58 +106,6 @@ namespace jokubui_uzd {
             if (b == 0) return num / den;
             if ((b & 1) == 1) return 1;
             return 0;
-        }
-    }
-    struct Complex {
-        public double Re, Im;
-        public double SqMag => Re * Re + Im * Im;
-        public double Mag => Math.Sqrt(SqMag);
-        public Complex(double re = 0d, double im = 0d) {
-            Re = re;
-            Im = im;
-        }
-        public static Complex Ln(Complex c) {
-            double im = Math.Atan2(c.Im, c.Re);
-            return new Complex() {
-                Re = Math.Log(c.SqMag) / 2d,
-                Im = (im >=0) ? im : im + Math.Tau,
-            };
-        }
-        public static Complex Exp(Complex c) {
-            return new Complex() {
-                Re = Math.Cos(c.Im) * Math.Exp(c.Re),
-                Im = Math.Sin(c.Im) * Math.Exp(c.Re),
-            };
-        }
-        public Complex() { Re = 0d; Im = 0d; }
-        public Complex Conjugate => new (Re, -Im );
-        public static Complex operator + (Complex left, Complex right) {
-            return new Complex { Im = left.Im + right.Im, Re = left.Re + right.Re };
-        }
-        public static Complex operator - (Complex complex) => new() { Re = -complex.Re, Im = -complex.Im };
-        public static Complex operator * (Complex left, Complex right) {
-            return new() {
-                Re = left.Re * right.Re - left.Im * right.Im,
-                Im = left.Im * right.Re + left.Re * right.Im
-            };
-        }
-        public static Complex operator * (Complex left, double right) {
-            return new() { Re = left.Re * right, Im = left.Im * right };
-        }
-        public static Complex operator /(Complex left, double right) => left * (1 / right); 
-        public static Complex operator /(Complex left, Complex right) {
-            return (left * right.Conjugate) / right.SqMag;
-        }
-        public static Complex Pow(Complex b, int e) {
-            Complex result = new() { Im = 0, Re = 1 };
-            for (;e > 0; e >>= 1) {
-                if ((e & 1) == 1) result *= b;
-                b *= b;
-            }
-            return result;
-        }
-        public static Complex Pow(Complex b, double e) {
-            return Exp(Ln(b) / e);
         }
     }
     static class Primes {
